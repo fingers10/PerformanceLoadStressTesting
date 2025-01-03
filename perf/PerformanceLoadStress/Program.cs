5154b63c445f52a68a5486ef0c67fb10e8f8c41a
abdulrahmanshabeekmohamed <abdulrahman.shabeekmohamed@rxglobal.com>
2025-01-03 18:44:39 +0530

    add perf project

---
+++ perf/PerformanceLoadStress/Program.cs
@@ -0,0 +1,20 @@
+﻿using NBomber.CSharp;
+
+using var httpClient = new HttpClient();
+
+var listScenario = Scenario.Create("get_weather_forecast", async context =>
+{
+    var response = await httpClient.GetAsync("https://localhost:5001/weatherforecast");
+
+    return response.IsSuccessStatusCode ? Response.Ok() : Response.Fail();
+})
+.WithoutWarmUp()
+.WithLoadSimulations(
+    Simulation.RampingInject(rate: 100,
+        interval: TimeSpan.FromSeconds(1),
+        during: TimeSpan.FromSeconds(30))
+);
+
+NBomberRunner
+    .RegisterScenarios(listScenario)
+    .Run();
